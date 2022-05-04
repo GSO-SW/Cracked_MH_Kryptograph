656ab715c6a705e802771b36497acf5ecc67b1d8
Deniz <kartal.deniiz@yahoo.com>
2022-05-04 12:50:12 +0200

    ok

---
+++ KryptographBibliothek/ZeichenZählen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KryptographBibliothek
+{
+    public class ZeichenZählen
+    {
+        public static void Zaehlen()
+        {
+            Console.WriteLine("hallo");
+        }
+    }
+}
