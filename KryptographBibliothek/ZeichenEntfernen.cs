9cac5a48de37239779494e80b698a29a693981e6
Nico Billig <nico.b37@gso.schule.koeln>
2022-05-04 12:37:44 +0200

    Update

---
+++ KryptographBibliothek/ZeichenEntfernen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KryptographBibliothek
+{
+    public class ZeichenEntfernen
+    {
+        public static void Zeichenentfernen()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
