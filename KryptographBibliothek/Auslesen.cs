db7c379c311bdba2b0ab49d9faa5d5c44920aac7
Emre-Can Zeren <emrecan.z2@gso.schule.koeln>
2022-05-06 12:41:13 +0200

    UD

---
+++ KryptographBibliothek/Auslesen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KryptographBibliothek
+{
+    public class Auslesen
+    {
+        public static void AuslesenChiffre()
+        {
+            Console.WriteLine("wir sind beim Auslesen");
+
+
+            string text = System.IO.File.ReadAllText(@"\\BK175055\emrecan.z2\Download\Chiffre.txt");
+            
+           System.Console.WriteLine("Inhalt der Datei Textlesen.txt = {0}", text);
+
+            
+        
+        }
+    }
+}
--- StartKryptograph/main.cs
+++ StartKryptograph/main.cs
@@ -7,6 +7,9 @@ namespace StartKryptograph
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            KryptographBibliothek.Auslesen.AuslesenChiffre();
+           
+
         }
     }
 }
